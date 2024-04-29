c83c3d8709928e4edd69cecda47df9abdf2dc85d
ncera32 <125378778+ncera32@users.noreply.github.com>
2024-04-29 05:23:36 -0400

    Update PostProcessing.py

--- PostProcessing.py
+++ PostProcessing.py
@@ -15,13 +15,28 @@ obj_vals_CONMIN = data['obj_vals_CONMIN']
 cl_con_vals_CONMIN = data['cl_con_vals_CONMIN']
 cm_con_vals_CONMIN = data['cm_con_vals_CONMIN']
 fc_cd_vals_CONMIN = data['fc_cd_vals_CONMIN']
+obj_vals_SLSQP = data['obj_vals_SLSQP']
+cl_con_vals_SLSQP = data['cl_con_vals_SLSQP']
+cm_con_vals_SLSQP = data['cm_con_vals_SLSQP']
+fc_cd_vals_SLSQP = data['fc_cd_vals_SLSQP']
+obj_vals_IPOPT = data['obj_vals_IPOPT']
+cl_con_vals_IPOPT = data['cl_con_vals_IPOPT'][:400]
+cm_con_vals_IPOPT = data['cm_con_vals_IPOPT'][:400]
+fc_cd_vals_IPOPT = data['fc_cd_vals_IPOPT'][:400]
 
-# Now you can use these arrays for plotting or any other analysis
+
+#Plotting
 
 fig1 = plt.figure(figsize = (12,12))
 real_fc_cd_vals_CONMIN = [np.real(fc_cd_val) for fc_cd_val in fc_cd_vals_CONMIN]
+real_fc_cd_vals_SLSQP = [np.real(fc_cd_val) for fc_cd_val in fc_cd_vals_SLSQP]
+real_fc_cd_vals_IPOPT = [np.real(fc_cd_val) for fc_cd_val in fc_cd_vals_IPOPT]
 iterations_CONMIN = np.arange(len(real_fc_cd_vals_CONMIN))
+iterations_SLSQP = np.arange(len(real_fc_cd_vals_SLSQP))
+iterations_IPOPT = np.arange(len(real_fc_cd_vals_IPOPT))
 plt.plot(iterations_CONMIN, real_fc_cd_vals_CONMIN, marker='.', lw=2, color='r')
+plt.plot(iterations_SLSQP, real_fc_cd_vals_SLSQP, marker ='.', lw=2, color='b')
+plt.plot(iterations_IPOPT, real_fc_cd_vals_IPOPT, marker ='.', lw=2, color='g')
 plt.xlabel('iterations')
 plt.ylabel('Cd value')
 plt.show()
@@ -29,8 +44,14 @@ plt.pause(60)
 
 fig2 = plt.figure(figsize = (12,12))
 real_cl_con_vals_CONMIN = [np.real(cl_con_val) for cl_con_val in cl_con_vals_CONMIN]
+real_cl_con_vals_SLSQP = [np.real(cl_con_val) for cl_con_val in cl_con_vals_SLSQP]
+real_cl_con_vals_IPOPT = [np.real(cl_con_val) for cl_con_val in cl_con_vals_IPOPT]
 iterations_CONMIN = np.arange(len(real_cl_con_vals_CONMIN))
+iterations_SLSQP = np.arange(len(real_cl_con_vals_SLSQP))
+iterations_IPOPT = np.arange(len(real_cl_con_vals_IPOPT))
 plt.plot(iterations_CONMIN, real_cl_con_vals_CONMIN, marker='.', lw=2, color='r')
+plt.plot(iterations_SLSQP, real_cl_con_vals_SLSQP, marker='.', lw=2, color='b')
+plt.plot(iterations_IPOPT, real_cl_con_vals_IPOPT, marker='.', lw=2, color='g')
 plt.xlabel('iterations')
 plt.ylabel('CL constraint violation')
 plt.show()
@@ -38,8 +59,14 @@ plt.pause(60)
 
 fig3 = plt.figure(figsize = (12,12))
 real_cm_con_vals_CONMIN = [np.real(cm_con_val) for cm_con_val in cm_con_vals_CONMIN]
+real_cm_con_vals_SLSQP = [np.real(cm_con_val) for cm_con_val in cm_con_vals_SLSQP]
+real_cm_con_vals_IPOPT = [np.real(cm_con_val) for cm_con_val in cm_con_vals_IPOPT]
 iterations_CONMIN = np.arange(len(real_cm_con_vals_CONMIN))
+iterations_SLSQP = np.arange(len(real_cm_con_vals_SLSQP))
+iterations_IPOPT = np.arange(len(real_cm_con_vals_IPOPT))
 plt.plot(iterations_CONMIN, real_cm_con_vals_CONMIN, marker='.', lw=2, color='r')
+plt.plot(iterations_SLSQP, real_cm_con_vals_SLSQP, marker='.', lw=2, color='b')
+plt.plot(iterations_IPOPT, real_cm_con_vals_IPOPT, marker='.', lw=2, color='g')
 plt.xlabel('iterations')
 plt.ylabel('CM constraint violation')
 plt.show()
